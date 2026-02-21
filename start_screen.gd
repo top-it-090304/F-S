559aa44c90a741c5752c61baa42cf1c01921226c
Yulia <yulia.shevtsova2005@gmail.com>
2026-02-21 16:33:47 +0600

    Add start screen with animation

---
+++ start_screen.gd
@@ -0,0 +1,25 @@
+extends Control
+
+## Экран запуска с анимацией увеличения фрукта
+
+@onready var fruit_sprite: Sprite2D = $FruitSprite
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var timer: Timer = $Timer
+
+var main_menu_scene = preload("res://main_menu.tscn")
+
+func _ready() -> void:
+	# Настройка начального состояния фрукта
+	fruit_sprite.scale = Vector2(0.1, 0.1)
+	fruit_sprite.modulate.a = 0.0
+	
+	# Запуск анимации увеличения
+	animation_player.play("fruit_grow")
+	
+	# Таймер для перехода в главное меню
+	timer.wait_time = 2.5
+	timer.timeout.connect(_on_timer_timeout)
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	get_tree().change_scene_to_packed(main_menu_scene)
